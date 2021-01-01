7afb0580965644e5eb4bcce1033cdfdbed72005e
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2021-01-01 22:39:35 +0900

    インデント修正

--- src/sheet/shared/Sheet.ts
+++ src/sheet/shared/Sheet.ts
@@ -5,37 +5,37 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 const credentials = require('../credentials.json');
 
 export type RowType = {
-	name: string
-	email: string
+  name: string
+  email: string
 }
 
 export default class Sheet {
-	update = async(row: RowType) => {
-		try {
+  update = async(row: RowType) => {
+    try {
 
-			const SPREADSHEET_ID = process.env.SPREADSHEET_ID || ''
-			const WORKSHEET_ID = process.env.WORKSHEET_ID || ''
+      const SPREADSHEET_ID = process.env.SPREADSHEET_ID || ''
+      const WORKSHEET_ID = process.env.WORKSHEET_ID || ''
 
-			console.log(SPREADSHEET_ID)
+      console.log(SPREADSHEET_ID)
 
-			const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
-			await doc.useServiceAccountAuth(credentials);
-			await doc.loadInfo();
+      const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+      await doc.useServiceAccountAuth(credentials);
+      await doc.loadInfo();
 
-			console.log(doc.title)
+      console.log(doc.title)
 
-			// シート特定
-			const sheet = await doc.sheetsById[WORKSHEET_ID]
-			// 書き込み
-			sheet.addRow(row);
+      // シート特定
+      const sheet = await doc.sheetsById[WORKSHEET_ID]
+      // 書き込み
+      sheet.addRow(row);
 
-		} catch (error) {
-			console.error('--------💣 エラー発生---------', error)
-			return 'error'
-		}
+    } catch (error) {
+      console.error('--------💣 エラー発生---------', error)
+      return 'error'
+    }
 
-		return 'success'
-	}
+    return 'success'
+  }
 }
 
 // デバッグ用
